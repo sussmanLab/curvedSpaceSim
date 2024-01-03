1007d418dc3d5590de3d893d3feba13c6516ae33
Daniel Sussman <dmsussman@gmail.com>
2024-01-03 15:01:44 -0500

    geodesic distances on meshes

--- inc/cgalIncludesAndTypedefs.h
+++ inc/cgalIncludesAndTypedefs.h
@@ -2,8 +2,8 @@
 #define CGAL_INCLUDES_TYPEDEFS_H
 
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Exact_predicates_exact_constructions_kernel_with_sqrt.h>
-#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+//#include <CGAL/Exact_predicates_exact_constructions_kernel_with_sqrt.h>
+//#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
 #include <CGAL/Surface_mesh.h>
 #include <CGAL/Surface_mesh_shortest_path.h>
 #include <CGAL/AABB_face_graph_triangle_primitive.h>
@@ -29,15 +29,31 @@ typedef K::Ray_3                                                        ray3;
 typedef K::Segment_3                                                    segment3;
 typedef K::Intersect_3                                                  intersect3;
 typedef K::Triangle_3                                                   triangle3;
-typedef CGAL::Surface_mesh<point3>                                     triangleMesh;
-typedef triangleMesh::Face_index                                       faceIndex;
-typedef triangleMesh::Halfedge_index                                   halfedgeIndex;
-typedef triangleMesh::Vertex_index                                     vertexIndex;
+typedef CGAL::Surface_mesh<point3>                                      triangleMesh;
+typedef triangleMesh::Face_index                                        faceIndex;
+typedef triangleMesh::Halfedge_index                                    halfedgeIndex;
+typedef triangleMesh::Vertex_index                                      vertexIndex;
+typedef CGAL::Surface_mesh_shortest_path_traits<K, triangleMesh>        Traits;
+typedef CGAL::Surface_mesh_shortest_path<Traits>                        surfaceMeshShortestPath;
+
+
 namespace CP = CGAL::parameters;
 namespace PMP = CGAL::Polygon_mesh_processing;
-typedef PMP::Barycentric_coordinates<FT>                                Barycentric_coordinates;
-typedef PMP::Face_location<triangleMesh, FT>                           faceLocation;
-typedef CGAL::Face_around_target_circulator<triangleMesh>              faceCirculator;
-typedef typename boost::graph_traits<triangleMesh>::vertex_descriptor           vertexDescriptor;
-typedef typename boost::graph_traits<triangleMesh>::face_descriptor             faceDescriptor;
+//typedef PMP::Barycentric_coordinates<FT>                                Barycentric_coordinates;
+//typedef PMP::Face_location<triangleMesh, FT>                            faceLocation;
+typedef CGAL::Face_around_target_circulator<triangleMesh>               faceCirculator;
+typedef typename boost::graph_traits<triangleMesh>::vertex_descriptor   vertexDescriptor;
+typedef typename boost::graph_traits<triangleMesh>::face_descriptor     faceDescriptor;
+
+typedef boost::graph_traits<triangleMesh>                               graphTraits;
+typedef graphTraits::vertex_iterator                                    vertexIterator;
+typedef graphTraits::face_iterator                                      faceIterator;
+
+typedef typename surfaceMeshShortestPath::Barycentric_coordinates       barycentricCoordinates;
+typedef typename surfaceMeshShortestPath::Face_location                 faceLocation;
+typedef surfaceMeshShortestPath::Shortest_path_result                   shortestPathResult;
+
+typedef CGAL::AABB_face_graph_triangle_primitive<triangleMesh>          AABB_face_graph_primitive;
+typedef CGAL::AABB_traits<K, AABB_face_graph_primitive>            AABB_face_graph_traits;
+typedef CGAL::AABB_tree<AABB_face_graph_traits>                         AABB_tree;
 #endif
--- src/models/triangulatedMeshSpace.cpp
+++ src/models/triangulatedMeshSpace.cpp
@@ -16,7 +16,38 @@ void triangulatedMeshSpace::loadMeshFromFile(std::string filename, bool verbose)
 
 void triangulatedMeshSpace::distance(meshPosition &p1, std::vector<meshPosition> &p2, std::vector<double> &distances, std::vector<vector3> &startPathTangent, std::vector<vector3> &endPathTangent)
     {
-    UNWRITTENCODE("all mesh space stuff unwritten");
+    int nTargets = p2.size();
+    distances.resize(nTargets);
+    startPathTangent.resize(nTargets);
+    endPathTangent.resize(nTargets);
+
+    surfaceMeshShortestPath pathFinder(surface);
+    AABB_tree tree;
+    pathFinder.build_aabb_tree(tree);
+
+    //shortestPaths needs barycentric coordinates... for now this entails a conversion step. Potentially motivates a switch so that the meshPosition data type is always this faceLocation structure?
+    faceLocation sourcePoint = pathFinder.locate<AABB_face_graph_traits>(p1.x,tree);
+    pathFinder.add_source_point(sourcePoint.first,sourcePoint.second);
+  
+    pathFinder.build_sequence_tree();
+
+    for(int ii = 0; ii < nTargets; ++ii)
+        {
+        //pathPoints holds the sequence of intersection points between the shortest path and the meshed surface (edges, vertices, etc)
+        std::vector<point3> pathPoints; 
+        faceLocation targetPoint = pathFinder.locate<AABB_face_graph_traits>(p2[ii].x,tree);
+        shortestPathResult geodesic = pathFinder.shortest_path_points_to_source_points(targetPoint.first, targetPoint.second,  std::back_inserter(pathPoints));
+        distances[ii] = std::get<0>(geodesic);
+        //Note that the path goes from the target to source, so if we want to know path tangent at the source for force calculation, we must use the *end* of points[]
+        int pathSize = pathPoints.size();
+        startPathTangent[ii] = vector3(pathPoints[pathSize-2],pathPoints[pathSize-1]);
+        endPathTangent[ii] = vector3(pathPoints[0],pathPoints[1]);
+        //normalize path tangents
+        double normalization = sqrt(startPathTangent[ii].squared_length());
+        startPathTangent[ii] /= normalization;
+        normalization = sqrt(endPathTangent[ii].squared_length());
+        endPathTangent[ii] /= normalization;
+        };
     };
 
 void triangulatedMeshSpace::displaceParticle(meshPosition &pos, vector3 &displacementVector)
