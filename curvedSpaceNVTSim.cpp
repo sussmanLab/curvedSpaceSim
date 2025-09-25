def821a42ad808a32c441a5f040c2218fafe3a56
dimsunlight <toler.webb@gmail.com>
2025-09-25 12:23:01 -0400

    updated curvedSpaceNVTSim to modern style

--- curvedSpaceNVTSim.cpp
+++ curvedSpaceNVTSim.cpp
@@ -42,14 +42,16 @@ int main(int argc, char*argv[])
     ValueArg<int> programBranchSwitchArg("z","programBranchSwitch","an integer controlling program branch",false,0,"int",cmd);
     ValueArg<int> particleNumberSwitchArg("n","number","number of particles to simulate",false,20,"int",cmd);
     ValueArg<int> iterationsArg("i","iterations","number of performTimestep calls to make",false,1000,"int",cmd);
-    ValueArg<int> saveFrequencyArg("s","saveFrequency","how often a file gets updated",false,100,"int",cmd);
+    ValueArg<int> saveFrequencyArg("f","saveFrequency","how often a file gets updated",false,100,"int",cmd);
     ValueArg<int> chainLengthArg("l", "M", "length of N-H chain", false, 2, "int", cmd); 
-    ValueArg<int> sourcesArg("k", "nSources", "number of particles to collect ditsances from", false, 1, "int", cmd); 
+    ValueArg<int> sourcesArg("k", "nSources", "number of particles to collect ditsances from", false, 1, "int", cmd);
+    ValueArg<int> sampleNoArg("c","sampleNo","sample identifier for this set of parameters",false,1,"int",cmd); 
     ValueArg<string> meshSwitchArg("m","meshSwitch","filename of the mesh you want to load",false,"../exampleMeshes/torus_isotropic_remesh.off","string",cmd);
-    ValueArg<double> areaFractionArg("f","areaFraction","percent of mesh area covered by particles",false,1.,"double",cmd);
+    ValueArg<string> savePathArg("s","fileSavePath","path to where data will be saved",false,"./","string",cmd);
+    ValueArg<double> areaFractionArg("a","areaFraction","percent of mesh area covered by particles",false,1.,"double",cmd);
     ValueArg<double> deltaTArg("e","dt","timestep size",false,.01,"double",cmd);
     ValueArg<double> temperatureArg("t","T","temperature to set",false,.2,"double",cmd);
-    
+
     SwitchArg reproducibleSwitch("r","reproducible","reproducible random number generation", cmd, true);
     SwitchArg dangerousSwitch("d","dangerousMeshes","meshes where submeshes are dangerous", cmd, false);
     SwitchArg verboseSwitch("v","verbose","output more things to screen ", cmd, false);
@@ -59,13 +61,15 @@ int main(int argc, char*argv[])
     //define variables that correspond to the command line parameters
     int programBranch = programBranchSwitchArg.getValue();
     int N = particleNumberSwitchArg.getValue();
-    int maximumIterations = iterationsArg.getValue();
+    int tMax = iterationsArg.getValue();
     int saveFrequency = saveFrequencyArg.getValue();
     int M = chainLengthArg.getValue(); 
     int nSources = sourcesArg.getValue();  
+    int sampleNo = sampleNoArg.getValue();
     string meshName = meshSwitchArg.getValue();
+    string savePath = savePathArg.getValue();
     double dt = deltaTArg.getValue();
-    double af = areaFractionArg.getValue();
+    double areaFraction = areaFractionArg.getValue();
     double temperature = temperatureArg.getValue();
     bool verbose= verboseSwitch.getValue();
     bool reproducible = reproducibleSwitch.getValue();
@@ -76,10 +80,10 @@ int main(int argc, char*argv[])
     meshSpace->useSubmeshingRoutines(false);
    
     double area = meshSpace->getArea();
-    double maximumInteractionRange = 2*sqrt(af*area/(N*M_PI));
+    double sigma = 2*sqrt(areaFraction*area/(N*M_PI));
    
     if(programBranch >0)
-        meshSpace->useSubmeshingRoutines(true,maximumInteractionRange,dangerous);
+        meshSpace->useSubmeshingRoutines(true,sigma,dangerous);
 
     shared_ptr<simpleModel> configuration=make_shared<simpleModel>(N);
     configuration->setVerbose(verbose);
@@ -87,7 +91,7 @@ int main(int argc, char*argv[])
     cout << "Area: " << meshSpace->getArea() << endl;
 
     //set up the cellListNeighborStructure, which needs to know how large the mesh is
-    shared_ptr<cellListNeighborStructure> cellList = make_shared<cellListNeighborStructure>(meshSpace->minVertexPosition,meshSpace->maxVertexPosition,maximumInteractionRange);
+    shared_ptr<cellListNeighborStructure> cellList = make_shared<cellListNeighborStructure>(meshSpace->minVertexPosition,meshSpace->maxVertexPosition,sigma);
     if(programBranch >= 1)
         configuration->setNeighborStructure(cellList);
 
@@ -96,8 +100,7 @@ int main(int argc, char*argv[])
     configuration->setRandomParticlePositions(noise);
     configuration->setMaxwellBoltzmannVelocities(noise,temperature);
 
-    //shared_ptr<gaussianRepulsion> pairwiseForce = make_shared<gaussianRepulsion>(1.0,.5);
-    shared_ptr<harmonicRepulsion> pairwiseForce = make_shared<harmonicRepulsion>(1.0,maximumInteractionRange);//stiffness and sigma. this is a monodisperse setting
+    shared_ptr<harmonicRepulsion> pairwiseForce = make_shared<harmonicRepulsion>(1.0,sigma);//stiffness and sigma. this is a monodisperse setting
     pairwiseForce->setModel(configuration);
 
     shared_ptr<simulation> simulator=make_shared<simulation>();
@@ -105,44 +108,50 @@ int main(int argc, char*argv[])
     simulator->addForce(pairwiseForce);
 
     //for now we fix timescale (tau) at 1.0
+    double tau = 1.0;
     cout << "number of N-H masses: " << M << endl; 
-    shared_ptr<noseHooverNVT> NVTUpdater=make_shared<noseHooverNVT>(dt, temperature, 1.0, M);
+    shared_ptr<noseHooverNVT> NVTUpdater=make_shared<noseHooverNVT>(dt, temperature, tau, M);
     NVTUpdater->setModel(configuration);
     simulator->addUpdater(NVTUpdater,configuration);
     
     profiler timer("various parts of the code");
 
     //by default, the simpleModelDatabase will save euclidean positions, mesh positions (barycentric + faceIdx), and particle velocities. See constructor for saving forces and/or particle types as well
-    simpleModelDatabase saveState(N,"./sphereNVT_testModelDatabase.h5",fileMode::replace);
-    saveState.writeState(configuration,0.0);
+    char outputFileName[512];
+    sprintf(outputFileName, "%s/NVT_N%i_areaFraction%.3f_sigma%.3f_tMax%i_mesh_%s_sample%i.h5",savePath.c_str(),N,areaFraction,sigma,tMax, meshName.c_str(),sampleNo);
+    std::cout << "Creating new HDF5 file: " << outputFileName << std::endl;
+    shared_ptr<simpleModelDatabase> saveState;
+    saveState = make_shared<simpleModelDatabase>(N,outputFileName,fileMode::replace); 
+    saveState->writeState(configuration,0.0);
 
     cout << "starting temp: " << NVTUpdater->getTemperatureFromKE() << endl; 
   
     ofstream temperatureFile("nvtTemperatures.csv"); 
- 
+
     double density = N/area; 
-    double rhoSigma2 = density*maximumInteractionRange*maximumInteractionRange; 
+    double rhoSigma2 = density*sigma*sigma; 
     
     double sumBPoR = 0;
-    int counter = 1; 
-        string distancesFilename = "../sphere_data/NVTrun_N" + to_string(N) + "_a"+to_string(maximumInteractionRange) + "_distances.csv";
+    int counter = 1;
+   
+    string distancesFilename = "../sphere_data/NVT_N" + to_string(N) + "_a"+to_string(sigma) + "_distances.csv";
     std::ofstream distancesFile(distancesFilename);
     double largeDouble = 10000.0;
     int bonusTime = 2; 
 
-    for (int ii = 0; ii < bonusTime*maximumIterations; ++ii)
+    for (int ii = 0; ii < bonusTime*tMax; ++ii)
         {
         timer.start();
         simulator->performTimestep();
         timer.end();
         if(ii%saveFrequency == saveFrequency-1)
             {
-            saveState.writeState(configuration,dt*ii);
+            saveState->writeState(configuration,dt*ii);
             double nowTemp = NVTUpdater->getTemperatureFromKE();
             printf("step %i T %f \n",ii,nowTemp);
             temperatureFile << ii << ", " << nowTemp << "\n";
 	    }
-	if ((ii > maximumIterations) && (ii%saveFrequency == saveFrequency-1))
+	if ((ii > tMax) && (ii%saveFrequency == saveFrequency-1))
        	    {
             double nowTemp = NVTUpdater->getTemperatureFromKE();
 	    vector<double> stress(9, 0.0); 
@@ -185,7 +194,7 @@ int main(int argc, char*argv[])
 		    }
 	        }
             distancesFile << "\n";
-	    meshSpace->useSubmeshingRoutines(true, maximumInteractionRange, dangerous);
+	    meshSpace->useSubmeshingRoutines(true, sigma, dangerous);
 	    }
         }
     
